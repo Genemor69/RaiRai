8d794c1b3443c944109e96cf03db4501d4781b17
DickGrayson <genemormuyangana@yahoo.com>
2025-02-06 23:52:13 +0800

    inshalla
    I hope ts works

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 
 const ValentineProposal = () => {
@@ -14,22 +16,11 @@ const ValentineProposal = () => {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "No", "Are you sure?", "Really sure?", "Think again!", "Last chance!", 
+      "Surely not?", "You might regret this!", "Give it another thought!", 
+      "Are you absolutely certain?", "This could be a mistake!", "Have a heart!", 
+      "Don't be so cold!", "Change of heart?", "Wouldn't you reconsider?", 
+      "Is that your final answer?", "You're breaking my heart ;(" 
     ];
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
@@ -38,7 +29,7 @@ const ValentineProposal = () => {
   const handleMouseMove = (e) => {
     if (isHovering && !yesPressed && noCount >= 2) {
       const noButton = document.getElementById("noButton");
-      if (!noButton) return; // Prevent errors if the button is not found
+      if (!noButton) return;
 
       const rect = noButton.getBoundingClientRect();
       const buttonCenter = {
